1dd83be22914de7326176acfb86dfbe999ee46e1
jordansbenjamin <14327@coderacademy.edu.au>
2023-06-21 11:04:37 +1000

    Made small adjustments to code layout and generate word when program is first oepened

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ data_dict = data.to_dict(orient='records')
 
 random_word = rand.choice(data_dict)
 
+# --------NEW WORD----------
+
+def new_card():
+    random_word = rand.choice(data_dict)
+    canvas.itemconfig(title_text, text="French")
+    canvas.itemconfig(word_text, text=random_word['French'])
 
 # --------UI SETUP--------- 
 
@@ -28,30 +34,23 @@ canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0
 card_front_img = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
 # Title text
-title_text = canvas.create_text(400, 150, text="Title", font=TITLE_FONT)
+title_text = canvas.create_text(400, 150, text="", font=TITLE_FONT)
 # Word text
 # word_text = canvas.create_text(400, 263, text="Word", font=WORD_FONT)
-word_text = canvas.create_text(400, 263, text=random_word['French'], font=WORD_FONT)
+word_text = canvas.create_text(400, 263, text="", font=WORD_FONT)
 
 canvas.grid(column=0, row=0, columnspan=2)
 
-# --------NEW WORD----------
-
-def new_word():
-    random_word = rand.choice(data_dict)
-    
-    canvas.itemconfig(word_text, text=random_word['French'])
-
-# --------UI SETUP CONT--------- 
-
 # Right button
 right_img = PhotoImage(file="./images/right.png")
-right_btn = Button(image=right_img, highlightthickness=0, borderwidth=0, bd=0, command=new_word)
+right_btn = Button(image=right_img, highlightthickness=0, borderwidth=0, bd=0, command=new_card)
 right_btn.grid(column=1, row=1)
 
 # Wrong button
 wrong_img = PhotoImage(file="./images/wrong.png")
-wrong_btn = Button(image=wrong_img, highlightthickness=0, borderwidth=0, command=new_word)
+wrong_btn = Button(image=wrong_img, highlightthickness=0, borderwidth=0, command=new_card)
 wrong_btn.grid(column=0, row=1)
+
+new_card()
 
 window.mainloop()
