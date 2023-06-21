0bf459c43db3fa47e24e8702629c24ab83cd7a75
jordansbenjamin <14327@coderacademy.edu.au>
2023-06-21 20:07:43 +1000

    Added logic for flipping cards to english side and implemented timer which automatically flips the card

--- main.py
+++ main.py
@@ -15,12 +15,27 @@ data_dict = data.to_dict(orient='records')
 
 random_word = rand.choice(data_dict)
 
-# --------NEW WORD----------
+# --------FLIP CARD--------
 
-def new_card():
+def flip_card(random_word):
+    # english_word = new_card()
+    canvas.itemconfig(canvas_card, image=card_back_img)
+    canvas.itemconfig(title_text, text="English",fill="white")
+    canvas.itemconfig(word_text, text=random_word["English"], fill="white")
+
+def new_word():
     random_word = rand.choice(data_dict)
-    canvas.itemconfig(title_text, text="French")
-    canvas.itemconfig(word_text, text=random_word['French'])
+    return random_word
+
+# --------NEW CARD----------
+
+def new_card(random_word):
+    global flip_timer
+    window.after_cancel(flip_timer)
+    canvas.itemconfig(canvas_card, image=card_front_img)
+    canvas.itemconfig(title_text, text="French", fill="black")
+    canvas.itemconfig(word_text, text=random_word['French'], fill="black")
+    flip_timer = window.after(3000, lambda:[flip_card(random_word)])
 
 # --------UI SETUP--------- 
 
@@ -28,11 +43,15 @@ window = Tk()
 window.title("Flash Card")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, lambda:[flip_card(new_word())])
+
 # Card canvas
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 # Card front img
 card_front_img = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+# Card back img
+card_back_img = PhotoImage(file="./images/card_back.png")
+canvas_card = canvas.create_image(400, 263, image=card_front_img)
 # Title text
 title_text = canvas.create_text(400, 150, text="", font=TITLE_FONT)
 # Word text
@@ -43,14 +62,16 @@ canvas.grid(column=0, row=0, columnspan=2)
 
 # Right button
 right_img = PhotoImage(file="./images/right.png")
-right_btn = Button(image=right_img, highlightthickness=0, borderwidth=0, bd=0, command=new_card)
+# right_btn = Button(image=right_img, highlightthickness=0, borderwidth=0, bd=0, command=new_card)
+right_btn = Button(image=right_img, highlightthickness=0, borderwidth=0, bd=0, command=lambda:[new_card(new_word())])
 right_btn.grid(column=1, row=1)
 
 # Wrong button
 wrong_img = PhotoImage(file="./images/wrong.png")
-wrong_btn = Button(image=wrong_img, highlightthickness=0, borderwidth=0, command=new_card)
+# wrong_btn = Button(image=wrong_img, highlightthickness=0, borderwidth=0, command=new_card)
+wrong_btn = Button(image=wrong_img, highlightthickness=0, borderwidth=0, command=lambda:[new_card(new_word())])
 wrong_btn.grid(column=0, row=1)
 
-new_card()
+new_card(new_word())
 
 window.mainloop()
